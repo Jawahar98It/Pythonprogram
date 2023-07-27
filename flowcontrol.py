ad0159ce9e9b279465a92b884b88466a62ae3fab
Jawahar98It <jawaharsumathi23@gmail.com>
2023-07-27 15:10:16 +0530

    flowcontrolprgm

---
+++ flowcontrol.py
@@ -0,0 +1,21 @@
+x = 20
+if x < 0:
+    print("Negative number")
+elif x % 2 == 0:
+    print("Positive even number")
+else:
+    print("Odd number")
+
+# using while loop
+i = 1
+n = 5
+sum = 0
+while i <= n:
+    sum = sum+i
+    i = i+1
+print(sum)
+
+# using for loop
+str1 = 'python'
+for i in str1:
+    print(i)
