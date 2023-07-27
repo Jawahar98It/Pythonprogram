8e5f041076c8466ebd8bd244138d2fdb05ab1c11
Jawahar98It <jawaharsumathi23@gmail.com>
2023-07-27 15:09:41 +0530

    dictionaryprgm

---
+++ dictpython.py
@@ -0,0 +1,33 @@
+balance = {"Jaimie": 25000,
+           "Cassie": 12000,
+           "Megan": 25700}
+print(balance)
+print(balance["Cassie"])
+print(balance.get("Megan"))
+balance["Cassie"] = 26900
+print(balance)
+print(len(balance))
+balance['Doroti'] = 25000
+print(balance)
+print("key value of dictionary", balance.keys())
+print("Jaimie" in balance)
+print("Ramya" in balance)
+print("Values in dictionary", balance.values())
+print("items methods", balance.items())
+print(balance.pop("Megan"))
+print(balance)
+print(balance.popitem())
+print(balance)
+del balance['Jaimie']
+print(balance)
+balance.clear()
+print(balance)
+balance1 = {"Jaimie": 25000,
+            "Cassie": 12000,
+            "Megan": 25700,
+            "Stuart": 23551,
+            "Kiba": 34900}
+balance2 = balance1.copy()
+print(balance2)
+balance2.update({'samuel': 50000})
+print(balance2, balance1)
